327e30a65f717aacdcd3c4cdddccc7c2ce8f0115
Vashistha Iyer <iy3rvashistha@gmail.com>
2018-07-17 19:21:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const getFields = require('./lib/get-fields')
+const generateFDF = require('./lib/generate-fdf')
+const generatePDF = require('./lib/generate-pdf')
+
+module.exports = { getFields, generateFDF, generatePDF }
